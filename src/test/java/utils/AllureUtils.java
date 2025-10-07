da1dcccb95049346b812f24524b3a4f1272d9237
ilyamamchyk <ilamamcik35@gmail.com>
2025-10-07 20:14:21 +0300

    Added cross-browser test

--- src/test/java/utils/AllureUtils.java
+++ src/test/java/utils/AllureUtils.java
@@ -8,11 +8,6 @@ import org.openqa.selenium.WebDriver;
 
 public class AllureUtils {
 
-    @Attachment(value = "screenshot", type = "image/png")
-    public static byte[] takeScreenshot(WebDriver driver) {
-        return ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
-    }
-
     @Attachment(value = "Screenshot", type = "image/png")
     public static byte[] takeScreenshot() {
         return ((TakesScreenshot) WebDriverRunner.getWebDriver())
